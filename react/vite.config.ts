d632a7b5c09c41cc2760561280386545bad52179
yanjike <2711396853@qq.com>
2024-08-02 21:20:03 +0800

    vite配置

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    open: true, // 自动打开
+    port: 8080, // 端口号
+  },
   plugins: [react()],
 })
